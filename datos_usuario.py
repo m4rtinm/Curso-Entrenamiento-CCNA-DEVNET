82b7072494c75782f8a4413f5ca2cbb1f3ffd2ca
Martin Muñoz <mart.munozt@profesor.duoc.cl>
2024-10-13 16:25:47 -0300

    Agregar script para solicitar datos de usuario

---
+++ datos_usuario.py
@@ -0,0 +1,10 @@
+# datos_usuario.py
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+edad = input("Ingrese su edad: ")
+sede = input("Ingrese su sede: ")
+
+print(f"Nombre: {nombre}")
+print(f"Apellido: {apellido}")
+print(f"Edad: {edad}")
+print(f"Sede: {sede}")
